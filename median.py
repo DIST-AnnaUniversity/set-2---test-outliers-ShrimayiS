3e7472fff942d77ac2b9186247fef8af23d2bbb2
ShrimayiS <112842665+ShrimayiS@users.noreply.github.com>
2022-12-29 15:32:52 +0530

    Update median.py

--- median.py
+++ median.py
@@ -5,12 +5,10 @@ import pandas as pd
 
 #read csv
 df = pd.read_csv('C:\\set-2---test-outliers-ShrimayiS-main\\dia.csv')
-
+df.sort_values("Pregnancies")
 #find the median of first column - Pregnancies
 median1 = df['Pregnancies'].median() 
 
 #print the value and round off to 2 decimal places
 
 print(round(median1, 2))
-
-
