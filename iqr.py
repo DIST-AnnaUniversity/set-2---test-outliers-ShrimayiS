4b6d3e5e58abf4cfd8301a31af321c2f243761a7
ShrimayiS <112842665+ShrimayiS@users.noreply.github.com>
2022-12-29 15:00:43 +0530

    Update iqr.py

--- iqr.py
+++ iqr.py
@@ -1,13 +1,20 @@
 import pandas as pd
+import numpy as np
 
 
 
 #read the csv file
+df = pd.read_csv('C:\\set-2---test-outliers-ShrimayiS-main\\dia.csv')
 
 
 #find the first quantile and third quantile
+Q3 = np.quantile(df['Pregnancies'], 0.75)
+Q1 = np.quantile(df['Pregnancies'], 0.25)
+print(Q1)
+print(Q3)
 
 #find iqr
+IQR = Q3 - Q1
+print(IQR)
 #round off iqr to two decimal places
-
-
+print(round(IQR, 2))
